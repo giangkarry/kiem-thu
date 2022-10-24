b110f85e4c4fbfbf25407ecee0c12fe54f11a9f0
Ddinh giang <62466416+giangkarry@users.noreply.github.com>
2022-10-24 10:57:14 +0700

    kiểm thử dòng dữ liệu

--- LuongHuu.java
+++ LuongHuu.java
@@ -59,18 +59,32 @@ public class LuongHuu {
     }
 
     /*============================KIỂM THỬ DÒNG ĐIỀU KHIỂN ==================================*/
+    // public int MucLuongHuu (int tuoi_nghi, int nam_bhxh, int tien_luong) {
+    //     int muc_luong;
+    //     double ty_le_luong;
+    //     if (tuoi_nghi < 0 || tuoi_nghi > 56 || nam_bhxh < 0 || nam_bhxh > 100 || tien_luong < 0 || tien_luong > 29800000) {
+    //         muc_luong = -1; // không hợp lệ
+    //     } else if (tuoi_nghi < 47 || nam_bhxh < 20) {
+    //         muc_luong = 0;
+    //     } else {
+    //         ty_le_luong = Math.max ((0.45 + (nam_bhxh - 20) * 0.02), 0.75);
+    //         muc_luong = (int) (tien_luong * ty_le_luong);
+    //     }
+    //     return muc_luong;
+    // }
+
+    /*=====================KIỂM THỬ DÒNG DỮ LIỆU=================== */
+    
     public int MucLuongHuu (int tuoi_nghi, int nam_bhxh, int tien_luong) {
-        int muc_luong;
-        double ty_le_luong;
+        int muc_luong = -2;
         if (tuoi_nghi < 0 || tuoi_nghi > 56 || nam_bhxh < 0 || nam_bhxh > 100 || tien_luong < 0 || tien_luong > 29800000) {
             muc_luong = -1; // không hợp lệ
         } else if (tuoi_nghi < 47 || nam_bhxh < 20) {
             muc_luong = 0;
         } else {
-            ty_le_luong = Math.max ((0.45 + (nam_bhxh - 20) * 0.02), 0.75);
+            double ty_le_luong = Math.max ((0.45 + (nam_bhxh - 20) * 0.02), 0.75);
             muc_luong = (int) (tien_luong * ty_le_luong);
         }
         return muc_luong;
     }
-    
 }
